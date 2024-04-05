a089047d68c16b3c1f96534895c64560d83ac167
Juan Sebastian Romero Piñeros <135473686+2722382@users.noreply.github.com>
2024-04-04 19:23:38 -0500

    Add files via upload

---
+++ ACTIVIDAD 2/proyecto.app.py
@@ -0,0 +1,34 @@
+# Definir Variables 
+
+Nombre  = "Sebastian"
+Edad = 18
+Estatura = 1,80
+
+
+# Operadores: operaciones matemáticas, lógicas y comparativas
+suma = 200 + 300
+resta = 100 - 50
+multiplicacion = 5 * 7 
+division = 9/3
+
+
+# Estructuras condicionales: if, elif y else
+else:entero
+    print("El entero es igual a 5.")
+
+# Bucle while: ejecutar bloques de código de forma repetitiva
+contador = 0
+while contador < 5:
+    print("Contador:", contador)
+    contador += 1
+
+# Bucle for: ejecutar bloques de código de forma repetitiva
+for i in range(5):
+    print("Iteración:", i)
+
+# Imprimir datos por consola usando la función print()
+print("Suma:", suma)
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
+print("División:", division)
+
